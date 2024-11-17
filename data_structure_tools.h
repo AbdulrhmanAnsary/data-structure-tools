7b5b1dd4ee2017dd9672615d189fbeb5ecf2c841
Abdulrhman Ansary <abdalrhmanmohamedansarymaky@gmail.com>
2024-11-17 15:20:40 +0200

    Subject: [Update] Update print for C-style
    Body:
    - Now print for C-style arrays supports strings.
    - Fix the strings assui.
    - I will add a the printing for multi-dimentional arrays very soon.

--- data_structure_tools.h
+++ data_structure_tools.h
@@ -12,7 +12,8 @@ class DataStructureTools
 {
   public:
     // For C-style arrays
-    void print(int container[], int size)
+    template <typename T>
+    void print(const T container[], int size)
     {
         std::cout << "{";
         for (int i = 0; i < size; i++)
